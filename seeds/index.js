20c1a7679ee29606b7734ea951c5c0ceb9a39263
Matthew Sanner <gravityskunk@gmail.com>
2022-11-07 20:23:19 -0600

    continuing to build

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDb= async() => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price= Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusantium illum omnis esse modi sequi sunt debitis, alias, architecto unde dolores! Quam esse, accusamus velit deserunt eaque natus tenetur et?',
+            price
         })
         await camp.save();
     }
