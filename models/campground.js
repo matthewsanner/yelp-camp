23665787bb5eb34528a5dd9e123075b1201aeb77
Matthew Sanner <gravityskunk@gmail.com>
2022-12-20 17:52:06 -0600

    added cluster map

--- models/campground.js
+++ models/campground.js
@@ -6,9 +6,14 @@ const ImageSchema = new Schema({
     url: String,
     filename: String
 })
+
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200')
 })
+
+// by default mongoose does not include virtuals when you convert a document to JSON, thus you need to set this option and pass it into the schema
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -36,7 +41,13 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 30)}...</p>`
+})
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
